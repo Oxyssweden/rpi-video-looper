29a22e2668c8131d4b4d0cee7942520df867ffd3
Andreas Radloff <andreas@oxys.se>
2020-06-09 16:49:45 +0200

    reload on update

--- update.sh
+++ update.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-cd /home/pi/rpi-video-looper && git pull
+cd /home/pi/rpi-video-looper && git pull | grep 'Updating' &> /dev/null
+if [ $? == 0 ]; then
+  # reload settings by restarting the video_looper
+  supervisorctl restart video_looper
+fi
 
-# todo restart if changes
-# reload settings by restarting the video_looper
-#supervisorctl restart video_looper
