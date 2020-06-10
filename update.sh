9d3d5885ea9414c392ed87ae1caef92a8e51327b
Andreas Radloff <andreas@oxys.se>
2020-06-10 10:41:14 +0200

    version bump

--- update.sh
+++ update.sh
@@ -2,6 +2,7 @@
 
 cd /home/pi/rpi-video-looper && git pull | grep 'Updating' &> /dev/null
 if [ $? == 0 ]; then
+  python3 /home/pi/rpi-video-looper/setup.py install --force
   # reload settings by restarting the video_looper
   supervisorctl restart video_looper
 fi
