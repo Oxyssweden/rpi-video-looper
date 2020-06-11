7d7f9ed154daca41e5faac2d403e6a2e500cd6ca
Andreas Radloff <andreas@oxys.se>
2020-06-11 17:56:22 +0200

    bug

--- Oxys_Video_Looper/vlc.py
+++ Oxys_Video_Looper/vlc.py
@@ -16,20 +16,22 @@ class VlcPlayer:
         time.sleep(2)
         self._vlc.connect()
         time.sleep(2)
-        self._vlc.loop()
         self._vlc.clear()
         self._vlc.enqueue(config.get('directory', 'path') + '/' + config.get('video_looper', 'loop'))
-        self.loop_index = self._vlc.search(config.get('video_looper', 'loop'))
         self._vlc.play()
+        self._vlc.loop()
+        self.loop_index = self._vlc.search(config.get('video_looper', 'loop'))
+        print("Loop index" + self.loop_index)
 
     def play(self, movie, **kwargs):
         """Play the provided movied file, if we are in the loop"""
         playing_index=self._vlc.playing_index()
+        print("PLAY! Now paying" + playing_index)
         if playing_index == self.loop_index:
             self._vlc.enqueue(self.config.get('directory', 'path') + '/' + movie.filename)
             index=self._vlc.search(movie.filename)
+            print("New index " + index)
             self._vlc.goto(index)
-            playlist=self._vlc.playlist()
             playing_index=index
             while playing_index==index:
                 time.sleep(2)
